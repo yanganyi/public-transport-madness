5fbf9afb03ff40a3e24e4bccf6ed23ed9c81eed4
yay <61081228+yanganyi@users.noreply.github.com>
2023-10-14 07:22:55 +0800

    initialisation

---
+++ main.py
@@ -0,0 +1,17 @@
+class Entity:
+    pass
+
+class Person(Entity):
+    pass
+
+class Bus(Entity):
+    pass
+
+class DoubleDecker(Bus):
+    pass
+
+class BusStop(Entity):
+    pass
+
+class Interchange(BusStop):
+    pass
